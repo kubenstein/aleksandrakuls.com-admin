4caf2481e9bd215d9398d7fdcd851355c98cc563
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-11-28 13:59:28 +0100

    working es6 webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,19 @@
 module.exports = {
-  entry: './src/scripts/main.js',
+  entry: './src/index.js',
   output: {
     path: __dirname + '/build',
     publicPath: '/build/',
     filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel',
+        query: {
+          presets: ['stage-0', 'es2015', 'react']
+        }
+      }
+    ]
   }
 }
