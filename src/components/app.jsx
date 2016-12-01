a951f90f3b2ede204ca7e314b3d84a1816c67cc6
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-12-01 18:13:29 +0100

    add footer and header

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ConcertList from './concert-list.jsx'
+import Header from './header.jsx'
+import Footer from './footer.jsx'
 
 export default class App extends React.Component {
   render() {
     return (
-      <ConcertList />
-    );
+      <div>
+        <Header />
+        <ConcertList />
+        <Footer />
+      </div>
+    )
   }
 }
