65f8d40b32ad09b4aa11f50d7f2bb25aa0340d42
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-12-02 14:11:14 +0100

    redirect to concerts as a default url

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, Redirect, hashHistory } from 'react-router'
 import AppLayout from './AppLayout.jsx'
 import ConcertList from './ConcertList.jsx'
 import ConcertEditPage from './ConcertEditPage.jsx'
@@ -8,6 +8,7 @@ export default class App extends React.Component {
   render() {
     return (
       <Router history={hashHistory}>
+        <Redirect from='/' to='/concerts' />
         <Route path='/' component={AppLayout}>
           <Route path='/concerts' component={ConcertList} />
           <Route path='/concerts/:concertId' component={ConcertEditPage} />
