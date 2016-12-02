9ba13b0cc02d42b894ca94e476cbd7e5903c7556
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-12-02 15:05:57 +0100

    edit form

--- src/components/ConcertEditPage.jsx
+++ src/components/ConcertEditPage.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router'
+import serialize from 'form-serialize'
 import ConcertsRepository from '../data/ConcertsRepository.jsx'
 
 export default class ConcertEditPage extends React.Component {
@@ -9,14 +11,28 @@ export default class ConcertEditPage extends React.Component {
     this.state = {concert: new ConcertsRepository().find(id)}
   }
 
+  submit(e) {
+    e.preventDefault();
+    const {id, date, textPL, textEN} = serialize(this.refs.form, {hash: true});
+    const concert = {id, date, textPL, textEN}
+
+    // TODO: do something
+    console.log(concert)
+  }
+
   render() {
     const concert = this.state.concert
     return (
       <div>
         <h1>Edit concert</h1>
-        date: {concert.date}<br />
-        textPL: {concert.textPL}<br />
-        textEN: {concert.textEN}<br />
+        <form className="form-body" autoComplete="off" onSubmit={(e) => { this.submit(e) }} ref='form'>
+          date    <input type="text" name="date" value={concert.date} onChange={() => {}} placeholder="date" /> <br />
+          textPL: <textarea name="textPL" value={concert.textPL} onChange={() => {}} placeholder="textPL" /> <br />
+          textEn: <textarea name="textEN" value={concert.textEN} onChange={() => {}} placeholder="textEN" /> <br />
+          <input type="hidden" name="id" value={concert.id} />
+          <input type="submit" value="Edit" />
+        </form>
+        <Link to='/concerts/'>Back</Link>
       </div>
     )
   }
