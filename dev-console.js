bdc99eb5ceed36471c5c75b2d80da76aa3db8f65
Jakub Niewczas <niewczas.jakub@gmail.com>
2016-12-21 14:34:00 +0100

    add mongo as a db

--- dev-console.js
+++ dev-console.js
@@ -6,24 +6,30 @@ const screen = blessed.screen();
 grid = new elements.grid({rows: 4, cols: 2, screen: screen})
 screen.render();
 
-const frontendWindow = grid.set(3, 0, 1, 1, elements.log, {
+const frontendWindow = grid.set(2, 0, 1, 1, elements.log, {
   fg: 'cyan',
   label: ' Webpack Dev Server ',
 });
 
-const backendWindow = grid.set(3, 1, 1, 1, elements.log, {
+const backendWindow = grid.set(2, 1, 1, 1, elements.log, {
   fg: 'green',
   label: ' Backend Server ',
 });
 
-const linterWindow = grid.set(0, 0, 3, 2, elements.log, {
+const linterWindow = grid.set(0, 0, 2, 2, elements.log, {
   fg: 'blue',
   label: ' Linter ',
+});
+
+const dbWindow = grid.set(3, 0, 1, 2, elements.log, {
+  fg: 'magenta',
+  label: ' MongoDB Server ',
 });
 
 writeToWindow(frontendWindow, 'npm', ['run', 'dev:frontend']);
 writeToWindow(backendWindow, 'npm', ['run', 'dev:backend']);
 writeToWindow(linterWindow, 'npm', ['run', 'dev:linter:ci']);
+writeToWindow(dbWindow, 'npm', ['run', 'dev:db']);
 
 
 // private
